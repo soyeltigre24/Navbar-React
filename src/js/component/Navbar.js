0a7227c092a7e36c0dd3ce525f96292a77c41b55
soyeltigre24 <Jairo.Ariza001@mymdc.net>
2021-03-04 23:39:24 +0000

    updated navbar

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -2,9 +2,59 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
 			<div className="container-fluid">
-				<span className="navbar-brand mb-0 h1">Navbar</span>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarTogglerDemo01"
+					aria-controls="navbarTogglerDemo01"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div
+					className="collapse navbar-collapse"
+					id="navbarTogglerDemo01">
+					<a className="navbar-brand" href="#">
+						Hidden brand
+					</a>
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<a
+								className="nav-link active"
+								aria-current="page"
+								href="#">
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a
+								className="nav-link active"
+								aria-current="page"
+								href="#">
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a
+								className="nav-link active"
+								aria-current="page"
+								href="#">
+								Home
+							</a>
+						</li>
+						<li className="nav-item ">
+							<a
+								className="nav-link active"
+								aria-current="page"
+								href="#">
+								Home
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -11,8 +11,23 @@ export function Home() {
 	return (
 		<div>
 			<Navbar />
-			<Jumbo />
-			<Cards />
+			<div className="container">
+				<Jumbo />
+				<div className="row">
+					<div className="col">
+						<Cards images="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlGO2N6UF0k2GWqU_qyYhGBuoxvpLVVreeQQ&usqp=CAU" />
+					</div>
+					<div className="col">
+						<Cards images="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTspESxDnxcqRDsz0w1HZ5IYqYXmSxbGpZBZw&usqp=CAU" />
+					</div>
+					<div className="col">
+						<Cards images="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlGO2N6UF0k2GWqU_qyYhGBuoxvpLVVreeQQ&usqp=CAU" />
+					</div>
+					<div className="col">
+						<Cards images="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlGO2N6UF0k2GWqU_qyYhGBuoxvpLVVreeQQ&usqp=CAU" />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
